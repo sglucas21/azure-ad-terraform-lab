025ccf232a4e7b4260b575e0d3a6eeb9356594d8
unknown <stevengarylucas@gmail.com>
2026-05-27 13:21:29 -0400

    Updated VM extensions to use GitHub raw script downloads

--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@ resource "azurerm_virtual_network" "vnet" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
-
-  dns_servers = ["10.0.1.10"]
+  #Remove this for now
+  #dns_servers = ["10.0.1.10"]
 }
 
 resource "azurerm_subnet" "subnet" {
@@ -62,8 +62,8 @@ resource "azurerm_network_interface" "dc_nic" {
   name                = "nic-ad-dc-01"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-
-  dns_servers = ["10.0.1.10"]
+  #Remove this for now
+  #dns_servers = ["10.0.1.10"]
 
   ip_configuration {
     name                          = "internal"
